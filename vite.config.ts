b384a846b519b7101c3286a489a6c53e8e60c07a
Evelyn Santos <evelyn.nsg@gmail.com>
2026-07-14 18:12:41 -0300

    ⚙️ Fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  build: { chunkSizeWarningLimit: 1000 }
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          firebase: ['firebase/app','firebase/auth','firebase/firestore','firebase/storage'],
+        }
+      }
+    }
+  }
 })
